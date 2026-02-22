28b7300122998b065627b060214b30e3ab11e335
HARISH K <146517340+harishkarna12@users.noreply.github.com>
2026-02-22 16:37:18 +0530

    Create question1.js

---
+++ question1.js
@@ -0,0 +1,6 @@
+Task 1: Normal Function to Arrow Function
+• Create a normal function called calculateSum that takes two numbers.
+• Return the sum and print when the function starts.
+• Convert the same function into an arrow function.
+• Call both functions and print outputs clearly.
+Example: calculateSum(5, 10) → Output: 15
